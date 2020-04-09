1504853c5cba2a2a259197aaa38b0ae5f370f90f
Susan Uland <sue.uland@gmail.com>
2020-04-08 19:32:46 -0700

    add data representation example code

---
+++ DataRepresent.java
@@ -0,0 +1,82 @@
+public class DataRepresent {
+
+    public static void main(String[] args) {
+        // Check our Recap Answers
+        // ints have 4 bytes
+        int data = 0b01100011;
+        System.out.println("Base 10(Decimal Value) is " + data);
+
+        // check shortcuts
+        data = 0b00111111;
+        System.out.println("Octal "+Integer.toOctalString(data));
+
+        data = 0b00111111;
+        System.out.println("Hex "+Integer.toHexString(data));
+
+        intDataType();
+        byteDataType();
+        longDataType();
+
+    }
+
+    // By default it uses 8 bytes to store the integer using twos complement
+    // MSB at the sign of the number  1- means neg 0-positive
+    // Intialize a long with a number greater than max int than you need  to
+    // add L at the end of number
+    private static void longDataType() {
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+
+        System.out.println("Max Long "+maxLong);
+        maxLong++;
+        System.out.println("Max Long ? "+maxLong);
+
+        long num = 2345678912L;
+    }
+    // By default the byte data type store numbers in the range -128 to 127
+    // using two's integer numbers
+    public static void byteDataType(){
+        byte maxByte = Byte.MAX_VALUE;
+        byte minByte = Byte.MIN_VALUE;
+
+        System.out.println("Max "+maxByte);
+        System.out.println("Min "+minByte);
+
+        // cause overflow add 1 to maxByte
+        maxByte++;
+        System.out.println("Max is? "+maxByte);
+
+        System.out.println(Integer.toBinaryString(maxByte));
+
+        byte num = (byte) 0b10101010;
+
+    }
+
+    // This method will play with the int data type
+    // int is using 4 bytes to store numbers as two's complement numbers
+
+    public static void intDataType() {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        System.out.println("Max int: " + max);
+        System.out.println("Min int: " + min);
+
+        // add 1 to max
+        max++;
+        System.out.println("Max Int overflowed: "+max);
+
+        // assign a hex number (0x) to the int
+        int num = 0xAFFAA23C;
+        System.out.println("Num is " + num);
+
+        // assign an octal number (0) to the int
+        num = 0721;
+        System.out.println("Num is " + num);
+
+        // assign a binary number (0b)
+        num = 0b0111000110;
+        System.out.println("Num as Binary: " + Integer.toBinaryString(num));
+    }
+
+}
